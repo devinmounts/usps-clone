b3c93aaef226066c3108275ca83068670438ede8
Devin Mounts <devinmounts@gmail.com>
2018-08-17 09:36:45 -0700

    create new flight class.

---
+++ src/app/models/flight.model.ts
@@ -0,0 +1,3 @@
+export class Flight{
+  constructor(public flightNumber: number, public departurePort: string, public arrivalPort: string, public departTime: DateTimeFormat, public arriveTime: DateTimeFormat){}
+}
