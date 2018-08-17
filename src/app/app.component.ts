2d364b9a7632c06c64c59a65fd3f659b8ef6cafb
Devin Mounts <devinmounts@gmail.com>
2018-08-17 09:37:34 -0700

    alter title and import Flight class.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Flight } from './models/flight.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Copa Air';
 }
