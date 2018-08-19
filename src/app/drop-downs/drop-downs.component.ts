91c4b14836f1b99ea443e8c6f4b45ae0a8d6fcb9
Devin Mounts <devinmounts@gmail.com>
2018-08-19 15:57:33 -0700

    toggle class styles and dropdown

--- src/app/drop-downs/drop-downs.component.ts
+++ src/app/drop-downs/drop-downs.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-drop-downs',
   templateUrl: './drop-downs.component.html',
   styleUrls: ['./drop-downs.component.css']
 })
-export class DropDownsComponent implements OnInit {
+export class DropDownsComponent {
 
-  constructor() { }
+  showInfo: boolean = false;
 
-  ngOnInit() {
+
+  showCollapsable(){
+    this.showInfo = !this.showInfo;
   }
 
+  toggleCollapser(){
+    if (this.showInfo === true){
+      return "collapser-style";
+    } else{
+      return "collapser-span";
+    }
+  }
+
+  // collapserStyle(){
+  //   if (this.showInfo === true){
+  //     return "collapser-style"
+  //   } else{
+  //     return "collapser-span"
+  //   }
+  // }
 }
