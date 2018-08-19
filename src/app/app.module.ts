e846fa888d2dfc4d52f69b6f8493366eeb7c11aa
Devin Mounts <devinmounts@gmail.com>
2018-08-19 13:35:10 -0700

    place input field and button in form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -22,7 +23,8 @@ import { FormComponent } from './form/form.component';
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
